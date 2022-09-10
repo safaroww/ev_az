3ce15a86590d66bb838be35055385709d48124f5
safaroww <asifseferov00@gmail.com>
2022-09-10 22:08:09 +0400

    ready for tasks:

--- info/models.py
+++ info/models.py
@@ -7,3 +7,7 @@ class Contact(models.Model):
     subject = models.CharField(max_length=100)
     message = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.subject
